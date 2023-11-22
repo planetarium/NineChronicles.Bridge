19018da4564f3e24444d0d84d60ee802e4c1e63f
moreal <dev.moreal@gmail.com>
2023-11-22 15:13:26 +0900

    Send error message with color

--- src/slack/messages/app-error-event.ts
+++ src/slack/messages/app-error-event.ts
@@ -3,7 +3,7 @@ import { ISlackMessage, SlackMessageSymbol } from ".";
 export class AppErrorEvent implements ISlackMessage {
     [SlackMessageSymbol] = true as const;
 
-    constructor(private readonly error?: Error) {}
+    constructor(private readonly error: Error) {}
 
     render() {
         if (this.error) {
@@ -12,18 +12,22 @@ export class AppErrorEvent implements ISlackMessage {
                 attachments: [
                     {
                         title: "Type",
+                        color: "#ff0033",
                         text: this.error.constructor.name,
                     },
                     {
                         title: "Name",
+                        color: "#ff0033",
                         text: this.error.name,
                     },
                     {
                         title: "Message",
+                        color: "#ff0033",
                         text: this.error.message,
                     },
                     {
                         title: "Stack",
+                        color: "#ff0033",
                         text: this.error.stack,
                     },
                 ],
