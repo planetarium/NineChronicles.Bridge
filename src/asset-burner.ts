da5711c3a87ed6f51fbd7f0074b17258889e7cb2
moreal <dev.moreal@gmail.com>
2023-11-28 14:54:48 +0900

    Share action building logic

--- src/asset-burner.ts
+++ src/asset-burner.ts
@@ -1,3 +1,4 @@
+import { Address } from "@planetarium/account";
 import { RecordView } from "@planetarium/bencodex";
 import { FungibleAssetValue, encodeCurrency } from "@planetarium/tx";
 import { IAssetBurner } from "./interfaces/asset-burner";
@@ -15,19 +16,27 @@ export class AssetBurner implements IAssetBurner {
     }
 
     async burn(amount: FungibleAssetValue, memo: string): Promise<string> {
-        const sender = (await this.signer.getAddress()).toBytes();
-        const action = new RecordView(
-            {
-                type_id: "burn_asset",
-                values: [
-                    sender,
-                    [encodeCurrency(amount.currency), amount.rawValue],
-                    memo,
-                ],
-            },
-            "text",
-        );
+        const sender = await this.signer.getAddress();
+        const action = encodeBurnAssetAction(sender, amount, memo);
 
         return await this.signer.sendTx(action);
     }
+}
+
+export function encodeBurnAssetAction(
+    sender: Address,
+    amount: FungibleAssetValue,
+    memo: string,
+): RecordView {
+    return new RecordView(
+        {
+            type_id: "burn_asset",
+            values: [
+                sender.toBytes(),
+                [encodeCurrency(amount.currency), amount.rawValue],
+                memo,
+            ],
+        },
+        "text",
+    );
 }
