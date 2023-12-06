4a610f599575ba6ff33f4260fcb36a5bd905d8d4
moreal <dev.moreal@gmail.com>
2023-12-06 19:04:31 +0900

    Wait efficiently

--- src/sync/processor.ts
+++ src/sync/processor.ts
@@ -1,25 +1,31 @@
 import { delay } from "../utils/delay";
 
+type Task = () => Promise<void>;
+
 export class Processor {
     private running: boolean;
 
-    constructor(private readonly tasks: (() => Promise<void>)[]) {}
+    constructor(
+        private readonly tasks: Task[],
+        private readonly intervalTasks: [number, Task][],
+    ) {}
 
     async start(): Promise<void> {
         this.running = true;
+
+        const timeouts: NodeJS.Timeout[] = [];
+        for (const [intervalMs, task] of this.intervalTasks) {
+            timeouts.push(setInterval(task, intervalMs));
+        }
 
         let cur = 0;
         while (this.running) {
             await this.tasks[cur]();
             cur = (cur + 1) % this.tasks.length;
+        }
 
-            try {
-                await delay(1000);
-            } catch {
-                console.error(
-                    "Ignore errors from calling delay() in Processor.start()",
-                );
-            }
+        for (const timeout of timeouts) {
+            clearInterval(timeout);
         }
 
         console.debug("Processor.start() loop ended");
--- src/sync/upstream/index.ts
+++ src/sync/upstream/index.ts
@@ -11,6 +11,7 @@ import {
 import { SlackBot } from "../../slack/bot";
 import { BridgeErrorEvent } from "../../slack/messages/bridge-error-event";
 import { BridgeEvent } from "../../slack/messages/bridge-event";
+import { delay } from "../../utils/delay";
 import { getTxId } from "../../utils/tx";
 import { dbTypeToSlackType, getNextBlockIndex, getNextTxNonce } from "../utils";
 import { responseTransactionsFromGarageEvents } from "./events/garage";
@@ -32,180 +33,191 @@ export async function processUpstreamEvents(
         await downstreamGQLClient.getGenesisHash(),
         "hex",
     );
-    const [
-        blockIndex,
-        responseTransactions,
-        unloadGarageEventsWithInvalidMemo,
-    ] = await client.$transaction(
-        async (tx) => {
-            const nextBlockIndex = await getNextBlockIndex(
-                tx,
-                upstreamNetworkId,
-                defaultStartBlockIndex,
-            );
-
-            const tipIndex = await upstreamGQLClient.getTipIndex();
-
-            if (nextBlockIndex >= tipIndex) {
-                console.debug(
-                    "[sync][upstream] skip. nextBlockIndex / tipIndex",
-                    nextBlockIndex,
-                    tipIndex,
+    const [isSkipped, responseTransactions, unloadGarageEventsWithInvalidMemo] =
+        await client.$transaction(
+            async (tx) => {
+                const nextBlockIndex = await getNextBlockIndex(
+                    tx,
+                    upstreamNetworkId,
+                    defaultStartBlockIndex,
                 );
-                return [nextBlockIndex - 1n, [], []];
-            }
 
-            console.debug("[sync][upstream] nextBlockIndex", nextBlockIndex);
-            console.log(
-                "[sync][upstream] networkId",
-                upstreamGQLClient.getPlanetID(),
-            );
+                const tipIndex = await upstreamGQLClient.getTipIndex();
 
-            await tx.block.create({
-                data: {
-                    networkId: upstreamGQLClient.getPlanetID(),
-                    index: nextBlockIndex,
-                },
-            });
+                if (nextBlockIndex >= tipIndex) {
+                    console.debug(
+                        "[sync][upstream] skip. nextBlockIndex / tipIndex",
+                        nextBlockIndex,
+                        tipIndex,
+                    );
+                    return [true, [], []];
+                }
 
-            console.debug("[sync][upstream] block row created.");
+                console.debug(
+                    "[sync][upstream] nextBlockIndex",
+                    nextBlockIndex,
+                );
+                console.log(
+                    "[sync][upstream] networkId",
+                    upstreamGQLClient.getPlanetID(),
+                );
 
-            const unloadGarageEvents = await getGarageUnloadEvents(
-                upstreamGQLClient,
-                agentAddress,
-                avatarAddress,
-                Number(nextBlockIndex),
-            );
-            const unloadGarageEventsWithValidMemo = unloadGarageEvents.filter(
-                (ev) => isValidParsedMemo(ev.parsedMemo),
-            );
-            const unloadGarageEventsWithInvalidMemo = unloadGarageEvents.filter(
-                (ev) => !isValidParsedMemo(ev.parsedMemo),
-            );
-            const transferAssetEvents = await getAssetTransferredEvents(
-                upstreamGQLClient,
-                agentAddress,
-                Number(nextBlockIndex),
-            );
-
-            console.debug(
-                "[sync][upstream] unloadGarageEvents.length",
-                unloadGarageEvents.length,
-            );
-            console.debug(
-                "[sync][upstream] unloadGarageEvents",
-                unloadGarageEvents,
-            );
-            console.debug(
-                "[sync][upstream] transferAssetEvents.length",
-                transferAssetEvents.length,
-            );
-            console.debug(
-                "[sync][upstream] transferAssetEvents",
-                transferAssetEvents,
-            );
-
-            await tx.requestTransaction.createMany({
-                data: [
-                    ...unloadGarageEventsWithValidMemo.map((ev) => {
-                        return {
-                            blockIndex: nextBlockIndex,
-                            networkId: upstreamNetworkId,
-                            sender: Address.fromHex(ev.signer, true).toString(),
-                            type: RequestType.UNLOAD_FROM_MY_GARAGES,
-                            category: RequestCategory.PROCESS,
-                            id: ev.txId,
-                        };
-                    }),
-                    ...transferAssetEvents.map((ev) => {
-                        return {
-                            blockIndex: nextBlockIndex,
-                            networkId: upstreamNetworkId,
-                            type: RequestType.TRANSFER_ASSET,
-                            sender: ev.sender.toString(),
-                            category: RequestCategory.PROCESS,
-                            id: ev.txId,
-                        };
-                    }),
-                ],
-            });
-
-            console.debug("[sync][upstream] request transaction rows created.");
-
-            const downstreamNextTxNonce = await getNextTxNonce(
-                tx,
-                downstreamGQLClient,
-                downstreamAccount,
-            );
-
-            console.debug(
-                "[sync][upstream] downstreamNextTxNonce",
-                downstreamNextTxNonce,
-            );
-
-            const responseTransactions = [
-                ...(await responseTransactionsFromGarageEvents(
-                    unloadGarageEventsWithValidMemo,
-                    downstreamAccount,
-                    downstreamNetworkId,
-                    downstreamGenesisHash,
-                    downstreamNextTxNonce,
-                    {
-                        agentAddress,
-                        avatarAddress,
+                await tx.block.create({
+                    data: {
+                        networkId: upstreamGQLClient.getPlanetID(),
+                        index: nextBlockIndex,
                     },
-                )),
-                ...(await responseTransactionsFromTransferEvents(
+                });
+
+                console.debug("[sync][upstream] block row created.");
+
+                const unloadGarageEvents = await getGarageUnloadEvents(
+                    upstreamGQLClient,
+                    agentAddress,
+                    avatarAddress,
+                    Number(nextBlockIndex),
+                );
+                const unloadGarageEventsWithValidMemo =
+                    unloadGarageEvents.filter((ev) =>
+                        isValidParsedMemo(ev.parsedMemo),
+                    );
+                const unloadGarageEventsWithInvalidMemo =
+                    unloadGarageEvents.filter(
+                        (ev) => !isValidParsedMemo(ev.parsedMemo),
+                    );
+                const transferAssetEvents = await getAssetTransferredEvents(
+                    upstreamGQLClient,
+                    agentAddress,
+                    Number(nextBlockIndex),
+                );
+
+                console.debug(
+                    "[sync][upstream] unloadGarageEvents.length",
+                    unloadGarageEvents.length,
+                );
+                console.debug(
+                    "[sync][upstream] unloadGarageEvents",
+                    unloadGarageEvents,
+                );
+                console.debug(
+                    "[sync][upstream] transferAssetEvents.length",
+                    transferAssetEvents.length,
+                );
+                console.debug(
+                    "[sync][upstream] transferAssetEvents",
                     transferAssetEvents,
+                );
+
+                await tx.requestTransaction.createMany({
+                    data: [
+                        ...unloadGarageEventsWithValidMemo.map((ev) => {
+                            return {
+                                blockIndex: nextBlockIndex,
+                                networkId: upstreamNetworkId,
+                                sender: Address.fromHex(
+                                    ev.signer,
+                                    true,
+                                ).toString(),
+                                type: RequestType.UNLOAD_FROM_MY_GARAGES,
+                                category: RequestCategory.PROCESS,
+                                id: ev.txId,
+                            };
+                        }),
+                        ...transferAssetEvents.map((ev) => {
+                            return {
+                                blockIndex: nextBlockIndex,
+                                networkId: upstreamNetworkId,
+                                type: RequestType.TRANSFER_ASSET,
+                                sender: ev.sender.toString(),
+                                category: RequestCategory.PROCESS,
+                                id: ev.txId,
+                            };
+                        }),
+                    ],
+                });
+
+                console.debug(
+                    "[sync][upstream] request transaction rows created.",
+                );
+
+                const downstreamNextTxNonce = await getNextTxNonce(
+                    tx,
+                    downstreamGQLClient,
                     downstreamAccount,
-                    downstreamNetworkId,
-                    downstreamGenesisHash,
-                    downstreamNextTxNonce +
-                        BigInt(unloadGarageEventsWithValidMemo.length),
-                )),
-            ];
+                );
 
-            console.debug(
-                "[sync][upstream] responseTransactions.length",
-                responseTransactions.length,
-            );
-            console.debug(
-                "[sync][upstream] responseTransactions",
-                responseTransactions,
-            );
+                console.debug(
+                    "[sync][upstream] downstreamNextTxNonce",
+                    downstreamNextTxNonce,
+                );
 
-            const responseTransactionsDBPayload = responseTransactions.map(
-                ({ signedTx, requestTxId, networkId, type }) => {
-                    const serializedTx = encode(encodeSignedTx(signedTx));
-                    const txid = getTxId(serializedTx);
-                    return {
-                        id: txid,
-                        nonce: signedTx.nonce,
-                        raw: Buffer.from(serializedTx),
-                        type,
-                        networkId: networkId,
-                        requestTransactionId: requestTxId,
-                    };
-                },
-            );
-            await tx.responseTransaction.createMany({
-                data: responseTransactionsDBPayload,
-            });
+                const responseTransactions = [
+                    ...(await responseTransactionsFromGarageEvents(
+                        unloadGarageEventsWithValidMemo,
+                        downstreamAccount,
+                        downstreamNetworkId,
+                        downstreamGenesisHash,
+                        downstreamNextTxNonce,
+                        {
+                            agentAddress,
+                            avatarAddress,
+                        },
+                    )),
+                    ...(await responseTransactionsFromTransferEvents(
+                        transferAssetEvents,
+                        downstreamAccount,
+                        downstreamNetworkId,
+                        downstreamGenesisHash,
+                        downstreamNextTxNonce +
+                            BigInt(unloadGarageEventsWithValidMemo.length),
+                    )),
+                ];
 
-            console.debug(
-                "[sync][upstream] response transaction rows created.",
-            );
+                console.debug(
+                    "[sync][upstream] responseTransactions.length",
+                    responseTransactions.length,
+                );
+                console.debug(
+                    "[sync][upstream] responseTransactions",
+                    responseTransactions,
+                );
 
-            return [
-                nextBlockIndex,
-                responseTransactionsDBPayload,
-                unloadGarageEventsWithInvalidMemo,
-            ];
-        },
-        {
-            timeout: 60 * 1000,
-        },
-    );
+                const responseTransactionsDBPayload = responseTransactions.map(
+                    ({ signedTx, requestTxId, networkId, type }) => {
+                        const serializedTx = encode(encodeSignedTx(signedTx));
+                        const txid = getTxId(serializedTx);
+                        return {
+                            id: txid,
+                            nonce: signedTx.nonce,
+                            raw: Buffer.from(serializedTx),
+                            type,
+                            networkId: networkId,
+                            requestTransactionId: requestTxId,
+                        };
+                    },
+                );
+                await tx.responseTransaction.createMany({
+                    data: responseTransactionsDBPayload,
+                });
+
+                console.debug(
+                    "[sync][upstream] response transaction rows created.",
+                );
+
+                return [
+                    false,
+                    responseTransactionsDBPayload,
+                    unloadGarageEventsWithInvalidMemo,
+                ];
+            },
+            {
+                timeout: 60 * 1000,
+            },
+        );
+
+    if (isSkipped) {
+        await delay(1000);
+    }
 
     for (const responseTransaction of responseTransactions) {
         await slackBot.sendMessage(
