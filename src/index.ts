c92044e5778603be7776943ca4db14798f5dd5d3
moreal <dev.moreal@gmail.com>
2023-12-18 11:40:58 +0900

    Deprecate non-rdb mode execution

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,19 @@ import { PrismaClient } from "@prisma/client";
 import { WebClient } from "@slack/web-api";
 import "dotenv/config";
 import { getAccountFromEnv } from "./accounts";
-import { AssetBurner } from "./asset-burner";
-import { AssetTransfer } from "./asset-transfer";
 import { getEnv, getRequiredEnv } from "./env";
 import { HeadlessGraphQLClient } from "./headless-graphql-client";
 import { IHeadlessGraphQLClient } from "./interfaces/headless-graphql-client";
-import { IMonitorStateStore } from "./interfaces/monitor-state-store";
-import { isBackgroundSyncTxpool } from "./interfaces/txpool";
-import { Minter } from "./minter";
-import { getMonitorStateHandler } from "./monitor-state-handler";
-import { AssetsTransferredMonitor } from "./monitors/assets-transferred-monitor";
-import { GarageUnloadMonitor } from "./monitors/garage-unload-monitor";
-import { AssetDownstreamObserver } from "./observers/asset-downstream-observer";
-import { AssetTransferredObserver } from "./observers/asset-transferred-observer";
-import { GarageObserver } from "./observers/garage-observer";
 import { PreloadHandler } from "./preload-handler";
-import { Signer } from "./signer";
 import { SlackBot } from "./slack/bot";
 import { SlackChannel } from "./slack/channel";
 import { AppStartEvent } from "./slack/messages/app-start-event";
 import { AppStopEvent } from "./slack/messages/app-stop-event";
-import { Sqlite3MonitorStateStore } from "./sqlite3-monitor-state-store";
 import { processDownstreamEvents } from "./sync/downstream";
 import { Processor } from "./sync/processor";
 import { stageTransactionFromDB } from "./sync/stage";
 import { updateTxStatuses } from "./sync/txresult";
 import { processUpstreamEvents } from "./sync/upstream";
-import { getTxpoolFromEnv } from "./txpool";
 import { Planet } from "./types/registry";
 
 const SLACK_BOT_USERNAME = getEnv("SLACK__BOT_USERNAME");
@@ -79,142 +65,13 @@ const slackBot: SlackBot | null =
             slackBot,
         );
     } else {
-        await withMonitors(
-            upstreamGQLClient,
-            downstreamGQLClient,
-            upstreamAccount,
-            downstreamAccount,
-            agentAddress,
-            avatarAddress,
-            slackBot,
-        );
+        throw new Error("Non RDB mode execution is depreacted.");
     }
 })().catch(async (error) => {
     console.error(error);
     await slackBot?.sendMessage(new AppStopEvent(error));
     process.exit(-1);
 });
-
-async function withMonitors(
-    upstreamGQLClient: IHeadlessGraphQLClient,
-    downstreamGQLClient: IHeadlessGraphQLClient,
-    upstreamAccount: Account,
-    downstreamAccount: Account,
-    agentAddress: Address,
-    avatarAddress: Address,
-    slackBot: SlackBot | null,
-) {
-    const monitorStateStore: IMonitorStateStore =
-        await Sqlite3MonitorStateStore.open(
-            getRequiredEnv("MONITOR_STATE_STORE_PATH"),
-        );
-
-    const upstreamAssetsTransferredMonitorMonitor =
-        new AssetsTransferredMonitor(
-            getMonitorStateHandler(
-                monitorStateStore,
-                "upstreamAssetTransferMonitor",
-            ),
-            upstreamGQLClient,
-            agentAddress,
-        );
-    const downstreamAssetsTransferredMonitorMonitor =
-        new AssetsTransferredMonitor(
-            getMonitorStateHandler(
-                monitorStateStore,
-                "downstreamAssetTransferMonitor",
-            ),
-            downstreamGQLClient,
-            agentAddress,
-        );
-    const garageMonitor = new GarageUnloadMonitor(
-        getMonitorStateHandler(
-            monitorStateStore,
-            "upstreamGarageUnloadMonitor",
-        ),
-        upstreamGQLClient,
-        agentAddress,
-        avatarAddress,
-    );
-
-    const upstreamGenesisBlockHash = await upstreamGQLClient.getGenesisHash();
-    const downstreamGenesisBlockHash =
-        await downstreamGQLClient.getGenesisHash();
-
-    const upstreamTxpool = getTxpoolFromEnv("NC_UPSTREAM", upstreamGQLClient);
-    const downstreamTxpool = getTxpoolFromEnv(
-        "NC_DOWNSTREAM",
-        downstreamGQLClient,
-    );
-
-    const upstreamSigner = new Signer(
-        upstreamAccount,
-        upstreamTxpool,
-        upstreamGenesisBlockHash,
-    );
-    const downstreamSigner = new Signer(
-        downstreamAccount,
-        downstreamTxpool,
-        downstreamGenesisBlockHash,
-    );
-
-    const minter = new Minter(downstreamSigner);
-
-    const upstreamTransfer = new AssetTransfer(upstreamSigner);
-    const downstreamBurner = new AssetBurner(downstreamSigner);
-
-    upstreamAssetsTransferredMonitorMonitor.attach(
-        new AssetTransferredObserver(slackBot, minter),
-    );
-
-    downstreamAssetsTransferredMonitorMonitor.attach(
-        new AssetDownstreamObserver(
-            slackBot,
-            upstreamTransfer,
-            downstreamBurner,
-        ),
-    );
-
-    garageMonitor.attach(
-        new GarageObserver(slackBot, minter, {
-            agentAddress,
-            avatarAddress,
-        }),
-    );
-
-    const handleSignal = () => {
-        console.log("Handle signal.");
-
-        upstreamAssetsTransferredMonitorMonitor.stop();
-        downstreamAssetsTransferredMonitorMonitor.stop();
-        garageMonitor.stop();
-
-        if (isBackgroundSyncTxpool(upstreamTxpool)) {
-            upstreamTxpool.stop();
-        }
-
-        if (isBackgroundSyncTxpool(downstreamTxpool)) {
-            downstreamTxpool.stop();
-        }
-
-        slackBot?.sendMessage(new AppStopEvent());
-    };
-
-    process.on("SIGTERM", handleSignal);
-    process.on("SIGINT", handleSignal);
-
-    upstreamAssetsTransferredMonitorMonitor.run();
-    downstreamAssetsTransferredMonitorMonitor.run();
-    garageMonitor.run();
-
-    if (isBackgroundSyncTxpool(upstreamTxpool)) {
-        upstreamTxpool.start();
-    }
-
-    if (isBackgroundSyncTxpool(downstreamTxpool)) {
-        downstreamTxpool.start();
-    }
-}
 
 async function withRDB(
     upstreamGQLClient: IHeadlessGraphQLClient,
